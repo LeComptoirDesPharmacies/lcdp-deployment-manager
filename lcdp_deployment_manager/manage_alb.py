71b48cb4ea864b59cac815fc0c7de247a155705c
Georges Faye <g.faye@lecomptoirdespharmacies.fr>
2020-10-28 17:50:33 +0100

    Adding maintenance-mode utils

--- lcdp_deployment_manager/manage_alb.py
+++ lcdp_deployment_manager/manage_alb.py
@@ -23,6 +23,22 @@ def get_alb_from_aws(alb_name):
     return alb_desc['LoadBalancers'][0]
 
 
+def get_alb_arn_by_name_contains(str_contained_in_name):
+    """
+    Récupère l'alb dont le nom contient une str
+    :param str_contained_in_name:    La str contenue dans le nom de l'alb
+    :type str_contained_in_name:     str
+    :return:            arn du load balancer trouvé
+    :rtype:             str
+    """
+    all_albs = elbv2_client.describe_load_balancers(LoadBalancerArns=[])
+    alb_arn = None
+    for alb in all_albs['LoadBalancers']:
+        if str_contained_in_name in alb['LoadBalancerName']:
+            alb_arn = alb['LoadBalancerArn']
+    return alb_arn
+
+
 def get_alb_target_group_arn(alb_arn, color, tg_type):
     """
     Récupère un target group ayant un type et une couleur précise
@@ -44,6 +60,28 @@ def get_alb_target_group_arn(alb_arn, color, tg_type):
             return tg['TargetGroupArn']
 
 
+def get_alb_rules_arns_by_hosts_starts_with(alb_arn, hosts_starts_with):
+    """
+    Récupère toutes les regles listener d'un alb
+    :param alb_arn: L'arn de l'alb
+    :type alb_arn: str
+    :param hosts_starts_with: Les debuts des noms des host des regles
+    :type: hosts_starts_with: list of str
+    :return: Les arns recherchés
+    :rtype: list of str
+    """
+    listeners = elbv2_client.describe_listeners(
+        LoadBalancerArn=alb_arn
+    )
+    public_rules_arns = []
+    if 'Listeners' in listeners:
+        for listener in listeners['Listeners']:
+            for host_starts_with in hosts_starts_with:
+                public_rules_arns.extend(get_listener_rules_by_host_starts_with(listener['ListenerArn'],
+                                                                                host_starts_with))
+    return public_rules_arns
+
+
 # ~~~~~~~~~~~~~~~~ Listener ~~~~~~~~~~~~~~~~
 
 def get_current_listener(alb_arn, ssl_enabled):
@@ -63,6 +101,30 @@ def get_production_color(listener):
     """
     current_target_group_arn = __get_default_forward_target_group_arn_from_listener(listener)
     return __get_color_from_resource(current_target_group_arn).upper()
+
+
+def get_listener_rules_by_host_starts_with(listener_arn, host_starts_with):
+    """
+    Récupère les règles d'un listener en fonction du host starts with
+    :param listener_arn:    listener actuel
+    :type listener_arn:     str
+    :param host_starts_with:    le debut de host recherché
+    :type host_starts_with:     str
+    :return:            Liste des arns des regles trouvées
+    :rtype:             list of str
+    """
+    all_rules = elbv2_client.describe_rules(
+        ListenerArn=listener_arn
+    )
+    public_rules_arns = []
+    if 'Rules' in all_rules:
+        for rule in all_rules['Rules']:
+            for condition in rule['Conditions']:
+                if 'Values' in condition:
+                    for rule_value in condition['Values']:
+                        if rule_value.startswith(host_starts_with):
+                            public_rules_arns.append(rule['RuleArn'])
+    return public_rules_arns
 
 
 def __get_listener(listeners, ssl_enabled):
@@ -123,5 +185,63 @@ def get_uncolored_rules(listener):
     return uncolored_rules
 
 
+def set_listener_rule_to_target_group(rule_arn, target_group_arn):
+    """
+    Assigne à une regle listener un target group
+    :param rule_arn:    la regle du listener
+    :type rule_arn:     str
+    :param target_group_arn:    le target group
+    :type target_group_arn:     str
+    """
+    return elbv2_client.modify_rule(
+        RuleArn=rule_arn,
+        Actions=[
+            {
+                'Type': 'forward',
+                'TargetGroupArn': target_group_arn
+            },
+        ]
+    )
+
+
 def __is_uncolored_host_header_value(value):
     return constant.BLUE not in value.upper() and constant.GREEN not in value.upper()
+
+
+# ~~~~~~~~~~~~~~~~ TARGET GROUP ~~~~~~~~~~~~~~~~
+
+def get_target_group_arn_by_name(target_group_name):
+    """
+    Get target group arn by target group name
+    :param target_group_name:
+    :type target_group_name: str
+    :return: target group arn or None if not found
+    :rtype: str
+    """
+    target_group_arn = None
+    target_groups = elbv2_client.describe_target_groups()
+    for target_group in target_groups['TargetGroups']:
+        if 'TargetGroupArn' in target_group \
+                and 'TargetGroupName' in target_group \
+                and target_group_name == target_group['TargetGroupName']:
+            target_group_arn = target_group['TargetGroupArn']
+    return target_group_arn
+
+
+def get_running_target_group_arn_by_name_contains(target_group_name_contains):
+    """
+    Recupere les targets groups (BLUE ou GREEN) actifs
+    :param target_group_name_contains: la chaine de caractères contenu dans le nom du target group
+    :type target_group_name_contains: str
+    :return:
+    """
+    target_group_arn = []
+    target_groups = elbv2_client.describe_target_groups()
+    for target_group in target_groups['TargetGroups']:
+        if 'TargetGroupArn' in target_group \
+                and 'TargetGroupName' in target_group \
+                and target_group_name_contains in target_group['TargetGroupName']:
+            targets = elbv2_client.describe_target_health(TargetGroupArn=target_group['TargetGroupArn'])
+            if targets['TargetHealthDescriptions'] and len(targets['TargetHealthDescriptions']) > 0:
+                target_group_arn.append(target_group['TargetGroupArn'])
+    return target_group_arn
