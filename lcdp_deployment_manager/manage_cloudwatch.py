1126612bb740c7927acd79cf877cfca7a04b8766
Antoine LCDP <antoine@lecomptoirdespharmacies.fr>
2024-10-25 09:32:24 +0200

    LDS-3985 :
    Tryout

--- lcdp_deployment_manager/manage_cloudwatch.py
+++ lcdp_deployment_manager/manage_cloudwatch.py
@@ -6,6 +6,14 @@ import logging
 
 cloudwatch_client = boto3.client('cloudwatch')
 
+def __search_expression(env, env_color, metric_name, aggregator):
+    return "SEARCH('{LCDP-SMUGGLER,LCDPEnvironment,ServiceVersion,SmugglerId} MetricName=\"{}\" LCDPEnvironment=\"{}\" ServiceVersion=\"{}\"', '{}', 30)".format(metric_name, env, env_color, aggregator),
+
+def __get_smugglers_metric_value(response, metric_name, aggregator):
+    values = [x["Values"][0] for x in response['MetricDataResults'] if x['Id'] == metric_name and len(x['Values']) > 0]
+    if len(values) > 0:
+        return aggregator(values)
+    return None
 
 def get_smuggler_metrics(env, env_color):
     end_time = datetime.now(timezone.utc)
@@ -15,45 +23,14 @@ def get_smuggler_metrics(env, env_color):
         MetricDataQueries=[
             {
                 'Id': 'active_jobs',
-                'MetricStat': {
-                    'Metric': {
-                        'Namespace': 'lcdp-smuggler',
-                        'MetricName': 'ActiveJobs',
-                        'Dimensions': [
-                            {
-                                'Name': 'Color',
-                                'Value': env_color
-                            },
-                            {
-                                'Name': 'env',
-                                'Value': env
-                            }
-                        ]
-                    },
-                    'Period': 30,
-                    'Stat': 'Maximum'
-                }
+                "Expression": __search_expression("ActiveJobs", env, env_color, 'Maximum'),
+                #"Expression": "SEARCH('Namespace=\"LCDP-SMUGGLER\" MetricName=\"ActiveJobs\" env=\"{}\" Color=\"{}\"', 'Maximum', 30)".format(env, env_color),
             },
             {
                 'Id': 'pending_jobs',
-                'MetricStat': {
-                    'Metric': {
-                        'Namespace': 'lcdp-smuggler',
-                        'MetricName': 'PendingJobs',
-                        'Dimensions': [
-                            {
-                                'Name': 'Color',
-                                'Value': env_color
-                            },
-                            {
-                                'Name': 'env',
-                                'Value': env
-                            }
-                        ]
-                    },
-                    'Period': 30,
-                    'Stat': 'Maximum'
-                }
+                "Expression": __search_expression("ActiveJobs", env, env_color, 'Maximum'),
+               # "Expression": "SEARCH('Namespace=\"LCDP-SMUGGLER\" MetricName=\"PendingJobs\" env=\"{}\" Color=\"{}\"', 'Maximum', 30)".format(
+               #     env, env_color),
             },
         ],
         StartTime=start_time,
@@ -63,22 +40,28 @@ def get_smuggler_metrics(env, env_color):
 
     metrics = dict()
 
-    logging.info("response")
-    logging.info(response)
+    #for result in response['MetricDataResults']:
+    #    if result['Id'] == 'active_jobs':
+
+
     try:
         # ['MetricDataResults'][0] : ActiveJobs
         # ['Values'][0] : Most recent value
-        active_jobs = response['MetricDataResults'][0]['Values'][0]
-        metrics['active_jobs'] = active_jobs
-    except (KeyError, IndexError, TypeError):
+        active_jobs = __get_smugglers_metric_value(response, 'active_jobs', max)
+        #active_jobs = response['MetricDataResults'][0]['Values'][0]
+        if active_jobs:
+            metrics['active_jobs'] = active_jobs
+    except (Exception):
         logging.exception("An error occured while retrieving 'active_jobs'")
 
     try:
         # ['MetricDataResults'][1] : PendingJobs
         # ['Values'][0] : Most recent value
-        pending_jobs = response['MetricDataResults'][1]['Values'][0]
-        metrics['pending_jobs'] = pending_jobs
-    except (KeyError, IndexError, TypeError):
+        pending_jobs = __get_smugglers_metric_value(response, 'pending_jobs', max)
+        #pending_jobs = response['MetricDataResults'][1]['Values'][0]
+        if pending_jobs:
+            metrics['pending_jobs'] = pending_jobs
+    except (Exception):
         logging.exception("An error occured while retrieving 'pending_jobs'")
 
     return metrics
